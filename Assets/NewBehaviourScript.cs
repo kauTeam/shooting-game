a208a22db3c4481ad27a9db1019eb26539a9ea78
junheui27 <junheui27@hanmail.net>
2017-05-13 17:41:58 +0900

    Revert "발표본"
    This reverts commit 6ed444dabc6293a089d91dd96e0a35ae3626933e.

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour {
     // Use this for initialization
 
-    bool state = false;
     void Start()
     {
 
@@ -19,17 +18,18 @@ public class NewBehaviourScript : MonoBehaviour {
 
     void Update()
     {
-        Invoke("changeScene", 3);
-        if (Input.GetKey(KeyCode.Return)||state)
+
+
+
+        if (Input.GetKey(KeyCode.Return))
         {
+
             Application.LoadLevel("start1 scene");
 
         }
 
         /* 엔터키를 누르면 main 신을 불러오라는 의미 */
-    }
-    void changeScene()
-    {
-        state = true;
+
+
     }
 }
