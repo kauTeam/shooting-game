9a5ffa6a9fa655b9abfbfdc37c602a866677d8cd
zjvlzld <jongsik1995@naver.com>
2017-05-18 15:29:25 +0900

    메인화면전환,초기화버튼
    구현완료

--- Assets/NewBehaviourScript3.cs
+++ Assets/NewBehaviourScript3.cs
@@ -25,4 +25,40 @@ public class NewBehaviourScript3 : MonoBehaviour {
     {
         SceneManager.LoadScene("scene/main scene");
     }
+	public void reset()
+	{
+		PlayerPrefs.SetInt ("lv",1);
+		PlayerPrefs.SetInt ("exp", 1);
+		PlayerPrefs.SetInt ("point", 1);
+
+		PlayerPrefs.SetFloat ("Damage", 1f);
+		PlayerPrefs.SetFloat ("Max_Damage", 5f);
+		PlayerPrefs.SetFloat ("attack_speed", 1f);
+		PlayerPrefs.SetFloat ("Max_attack_speed", 5f);
+		PlayerPrefs.SetFloat ("Mana", 1f);
+		PlayerPrefs.SetFloat ("Max_Mana", 5f);
+		PlayerPrefs.SetFloat ("Mana_re", 1f);
+		PlayerPrefs.SetFloat ("Max_Mana_re", 5f);
+		PlayerPrefs.SetFloat ("Hp", 1f);
+		PlayerPrefs.SetFloat ("Max_Hp", 5f);
+
+		PlayerPrefs.SetInt ("coin", 0);
+		PlayerPrefs.SetInt ("soul", 0);
+
+		PlayerPrefs.SetInt ("attack", 1);
+		PlayerPrefs.SetInt ("defence", 1);
+
+		PlayerPrefs.SetInt ("Metemor", 1);
+		PlayerPrefs.SetInt ("Poison", 1);
+		PlayerPrefs.SetInt ("FireBall", 1);
+		PlayerPrefs.SetInt ("IceGun", 1);
+		PlayerPrefs.SetInt ("HellDrop", 1);
+
+		PlayerPrefs.SetInt ("Barrier", 1);
+		PlayerPrefs.SetInt ("Shild", 1);
+		PlayerPrefs.SetInt ("Heal", 1);
+		PlayerPrefs.SetInt ("DotHeal", 1);
+		PlayerPrefs.SetInt ("Reflect", 1);
+
+	}
 }
