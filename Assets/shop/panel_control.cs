5bee055ebe3b7af475a5a16aefe087dc09da8e0b
zjvlzld <jongsik1995@naver.com>
2017-05-23 11:31:36 +0900

    5월 23일
    shop화면 전환

---
+++ Assets/shop/panel_control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_control : MonoBehaviour {
+
+
+	public GameObject soul;
+	public GameObject stat;
+	public GameObject skill;
+	// Use this for initialization
+
+	public void skill_on()
+	{
+		soul.SetActive (false);
+		stat.SetActive (false);
+		skill.SetActive (true);
+	}
+	public void stat_on()
+	{
+		soul.SetActive (false);
+		stat.SetActive (true);
+		skill.SetActive (false);
+
+	}
+	public void soul_on()
+	{
+		soul.SetActive (true);
+		stat.SetActive (false);
+		skill.SetActive (false);
+
+	}
+}
