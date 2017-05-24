bbcff528475f10db743a22e76cddbc1fa8b2499c
zjvlzld <jongsik1995@naver.com>
2017-05-24 21:14:55 +0900

    이미지 변경까지

---
+++ Assets/player/Button_img_control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Button_img_control : MonoBehaviour {
+
+
+	public Sprite[] Attack_sk_img =new Sprite[5];
+	public Sprite[] Defence_sk_img =new Sprite[5];
+
+	public GameObject attack_img;
+	public GameObject defence_img;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int attack = PlayerPrefs.GetInt ("attack");
+		int defence = PlayerPrefs.GetInt ("defence");
+		attack_img.GetComponent<Image> ().sprite = Attack_sk_img [attack-1];
+		defence_img.GetComponent<Image> ().sprite = Defence_sk_img [defence-1];
+	}
+}
